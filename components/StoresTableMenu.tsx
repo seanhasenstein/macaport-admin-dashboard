dd6e285cb1030330b0de2bfa37ac36a0b9a34f7d
Sean Hasenstein <seanhasenstein@gmail.com>
2022-01-25 16:33:38 -0600

    add external store link to stores table menu

--- components/StoresTableMenu.tsx
+++ components/StoresTableMenu.tsx
@@ -90,6 +90,27 @@ export default function StoresTableMenu({ storeId }: Props) {
             Edit Store
           </a>
         </Link>
+        <a
+          href={`${process.env.NEXT_PUBLIC_DOMAIN}/store/${storeId}`}
+          target="_blank"
+          rel="noreferrer"
+          className="link"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+            />
+          </svg>
+          Go to Store
+        </a>
       </div>
     </StoresTableMenuStyles>
   );
