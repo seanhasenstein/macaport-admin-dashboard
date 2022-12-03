6846a353869c6b8f556fd9a20c58f67e91ca3ada
Sean Hasenstein <seanhasenstein@gmail.com>
2022-12-03 11:13:29 -0600

    fix bug with homepage storesTable onSuccess query

--- hooks/useHomepageData.tsx
+++ hooks/useHomepageData.tsx
@@ -1,15 +1,8 @@
-import { useQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
 import { fetchHomepageData } from '../queries/stores';
 
 export default function useHomepageData() {
-  const queryClient = useQueryClient();
-
   return useQuery(['stores', 'homepage'], fetchHomepageData, {
-    onSuccess: data => {
-      data.stores.forEach(store => {
-        queryClient.setQueryData(['stores', 'store', store._id], store);
-      });
-    },
     staleTime: 1000 * 60 * 10,
   });
 }
