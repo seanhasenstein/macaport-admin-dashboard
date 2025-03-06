4473c0e5c83a222509d82fca1381cd25861f4601
Sean Hasenstein <seanhasenstein@gmail.com>
2025-03-06 15:52:20 -0600

    update edit store copy

--- components/store/StoreMenu.tsx
+++ components/store/StoreMenu.tsx
@@ -287,7 +287,7 @@ export default function StoreMenu({
                 d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
               />
             </svg>
-            Edit store
+            Edit this store
           </a>
         </Link>
 
